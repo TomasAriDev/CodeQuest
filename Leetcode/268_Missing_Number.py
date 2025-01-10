1b080454efa9bedb8981788a03862c3a9c8899e0
Nyph_Dx <tomas.ubilla@ug.uchile.cl>
2025-01-09 23:24:31 -0300

    Chore: Add documents.

---
+++ Leetcode/268_Missing_Number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums_ = set(nums)
+        i = 0
+        while i<n:
+            if {i}.issubset(nums_):
+                i+=1
+            else:
+                break
+        return i
+    
+# Usando el metodo de set 'issubset' es posible preguntar si
+# i esta en nums_, si no lo esta es muy facil saber cual es el que falta
